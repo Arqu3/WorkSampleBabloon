b1381322fb798854fbb3736ba04b6000b6b284b4
Ludwig <ludwiggustavsson3@gmail.com>
2017-12-03 18:11:18 +0100

    Added camera controller, updated player movement

---
+++ 3rdPersonCombat/Assets/Scripts/CameraController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ThirdPerson.CameraControll
+{
+    /// <summary>
+    /// Controlls camera movement
+    /// </summary>
+    public class CameraController : MonoBehaviour
+    {
+        #region Exposed fields
+
+        [Header ("Offset variables")]
+        [SerializeField]
+        Vector3 m_Offset = Vector3.zero;
+
+        [Header ("Input variables")]
+        [SerializeField]
+        Vector2 m_Sensitivity = new Vector2 (5.0f, 5.0f);
+        [SerializeField]
+        [Range (0.0f, 180.0f)]
+        float m_ClampY = 80.0f;
+
+        #endregion
+
+        #region Private fields
+
+        //Rotation variables
+        float m_AbsoluteX = 0.0f;
+        float m_AbsoluteY = 45.0f;
+
+        //Position variables
+        Transform m_PlayerTransform;
+
+        #endregion
+
+        void Start ()
+        {
+            m_PlayerTransform = Toolbox.Instance.PlayerTransform;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        void Update ()
+        {
+            m_AbsoluteX += Input.GetAxis ("Mouse X") * m_Sensitivity.x * Time.deltaTime;
+            if ( m_AbsoluteX > 360.0f ) m_AbsoluteX -= 360.0f;
+            else if ( m_AbsoluteX < -360.0f ) m_AbsoluteX += 360.0f;
+
+            m_AbsoluteY = Mathf.Clamp (m_AbsoluteY + Input.GetAxis ("Mouse Y") * -1 * m_Sensitivity.y * Time.deltaTime, -m_ClampY / 5.0f, m_ClampY);
+
+            Quaternion rot = Quaternion.Euler (m_AbsoluteY, m_AbsoluteX, 0.0f);
+            transform.position = m_PlayerTransform.position - ( rot * m_Offset );
+            transform.LookAt (m_PlayerTransform.position + rot * new Vector3 (m_Offset.x, m_Offset.y, 0.0f));
+        }
+    }
+}
--- 3rdPersonCombat/Assets/Scripts/PlayerController.cs
+++ 3rdPersonCombat/Assets/Scripts/PlayerController.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using ThirdPerson.Entity;
+using UnityEngine.SceneManagement;
 
 namespace ThirdPerson.Player
 {
+    /// <summary>
+    /// Central player controller script
+    /// </summary>
     [RequireComponent(typeof(Rigidbody), typeof(Collider))]
     public class PlayerController : BaseEntity
     {
@@ -29,6 +33,7 @@ namespace ThirdPerson.Player
         [Header ("Raycast variables")]
         [SerializeField]
         LayerMask m_GroundMask = Physics.AllLayers;
+        LayerMask m_WallMask = Physics.AllLayers;
 
         #endregion
 
@@ -41,9 +46,16 @@ namespace ThirdPerson.Player
 
         #region Private fields
 
+        //Stat variables
         int m_CurrentHealth = 0;
         int m_CurrentDamage = 0;
+
+        //Air variables
         bool m_JumpCooldown = false;
+        bool m_Grounded = false;
+
+        //Rotation variables
+        Quaternion m_Rotation;
 
         #endregion
 
@@ -61,7 +73,11 @@ namespace ThirdPerson.Player
 
         void Update ()
         {
+            if ( Input.GetKeyDown (KeyCode.Escape) ) Application.Quit ();
+
             Move ();
+
+            if ( Input.GetMouseButton (0) ) Attack ();
         }
 
         public override void ChangeHealh ( int amount )
@@ -72,7 +88,7 @@ namespace ThirdPerson.Player
 
         public override void Kill ()
         {
-            
+            SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
         }
 
         protected override void Attack ()
@@ -81,10 +97,24 @@ namespace ThirdPerson.Player
 
         protected override void Move ()
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            m_Grounded = Grounded;
+            m_Body.useGravity = !m_Grounded;
+
+            if (Input.GetKey(KeyCode.Space))
             {
-                if ( Grounded ) Jump (Vector3.up * m_JumpForce);
+                if ( m_Grounded ) Jump (Vector3.up * m_JumpForce);
             }
+
+            Vector3 forward = Camera.main.transform.forward;
+            forward.y = 0.0f;
+            m_Rotation = Quaternion.LookRotation (forward, Vector3.up);
+
+            if ( InputActive ) transform.rotation = Quaternion.Lerp (transform.rotation, Quaternion.LookRotation (forward, Vector3.up), 10.0f * Time.deltaTime);
+
+            Vector3 input = m_Rotation * new Vector3 (Input.GetAxis ("Horizontal"), 0.0f, Input.GetAxis ("Vertical"));
+            if ( WallCheck (input) ) input = Vector3.zero;
+
+            if (Grounded) m_Body.velocity = new Vector3 (input.x * m_MovementSpeed, m_Body.velocity.y, input.z * m_MovementSpeed);
         }
 
         bool Grounded
@@ -95,10 +125,35 @@ namespace ThirdPerson.Player
             }
         }
 
+        bool WallCheck(Vector3 direction)
+        {
+            int numRaycasts = 5;
+            Vector3 pos = m_Collider.bounds.center - new Vector3 (0.0f, m_Collider.bounds.size.y / 2.0f, 0.0f);
+            float height = m_Collider.bounds.size.y / numRaycasts;
+            RaycastHit hit;
+            for ( int i = 0 ; i < numRaycasts + 1 ; ++i )
+            {
+                //Debug.DrawRay (pos + new Vector3 (0.0f, height * i, 0.0f), direction, Color.red);
+                if ( Physics.Raycast (pos + new Vector3 (0.0f, height * i, 0.0f), direction, out hit, m_Collider.bounds.extents.x * 1.1f, m_WallMask, QueryTriggerInteraction.Ignore) )
+                {
+                    if (Vector3.Angle(hit.normal, Vector3.up) >= 89f) return true;
+                }
+            }
+            return false;
+        }
+
+        bool InputActive
+        {
+            get
+            {
+                return Mathf.Abs (Input.GetAxis ("Horizontal")) > 0.0f || Mathf.Abs (Input.GetAxis ("Vertical")) > 0.0f;
+            }
+        }
+
         void Jump(Vector3 force)
         {
             if ( m_JumpCooldown ) return;
-            StartCoroutine (JumpCooldown (0.5f));
+            StartCoroutine (JumpCooldown (0.1f));
             m_Body.AddForce (force, ForceMode.Impulse);
         }
 
